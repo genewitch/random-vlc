88c61d376d9af07619f545a32b7942235d29ec84
genewitch <genewitch@gmail.com>
2016-08-09 04:28:15 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+//microsoft compatible vlc randomizer
+
+class FileObject
+{
+system.filesystemobject o;
+
+public void new(obj FOLDERLIST)
+{
+foreach folder in FOLDERLIST
+{
+	foreach file in folder
+	{
+		if file.typeinfo = "VLC MEDIA FILE" //HERE IS THE EDITTTTTTTTTTTTTTTTTTTTTTTTT
+		{	
+			o.adduri(file)
+		}
+	}
+}
+}
+
+public void playRandom()
+{
+	int r = random(0, o.length())
+	int c = 0
+	foreach video in o
+	{
+		if c++ = r
+		{
+			EXEC vlc.exe o.uri
+		}
+	}
+}
